3f946273d8d3de2e127df612717b5df27ed504d1
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2023-02-07 16:06:34 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,45 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+List<string> content = new List<string>();
+bool startWriting = false;
 
-namespace Journal
+Console.WriteLine("Enter 'start' to start writing in the journal.");
+Console.WriteLine("Enter 'stop' to stop writing in the journal.");
+
+while (true)
 {
-    class Program
+    string line = Console.ReadLine().Trim();
+    if (line == "start")
     {
-        static void Main(string[] args)
-        {
-            List<string> content = new List<string>();
-            bool startWriting = false;
-
-            Console.WriteLine("Enter 'start' to start writing in the journal.");
-            Console.WriteLine("Enter 'stop' to stop writing in the journal.");
-
-            while (true)
-            {
-                string line = Console.ReadLine().Trim();
-                if (line == "start")
-                {
-                    startWriting = true;
-                }
-                else if (line == "stop")
-                {
-                    startWriting = false;
-                    break;
-                }
-                else if (startWriting)
-                {
-                    content.Add(line);
-                }
-            }
-
-            string currentDate = DateTime.Now.ToString("yyyy-MM-dd");
-            //string fileName = @"C:\Users\ericm\source\repos\Jean - Luc Picard’s journal" + currentDate + ".txt";
-
-            string header = "Captain's log\nStardate " + currentDate + "\n";
-            string body = string.Join("\n", content.ToArray());
-            string footer = "\nJean-Luc Picard";
-
-            File.WriteAllText(@"C:\Users\ericm\source\repos\Jean - Luc Picard’s journal" + currentDate + ".txt", header + body + footer);
-        }
+        startWriting = true;
+    }
+    else if (line == "stop")
+    {
+        startWriting = false;
+        break;
+    }
+    else if (startWriting)
+    {
+        content.Add(line);
     }
 }
+
+string currentDate = DateTime.Now.ToString("yyyy-MM-dd");
+//string fileName = @"C:\Users\ericm\source\repos\Jean - Luc Picard’s journal" + currentDate + ".txt";
+
+string header = "Captain's log\nStardate " + currentDate + "\n";
+string body = string.Join("\n", content.ToArray());
+string footer = "\nJean-Luc Picard";
+
+File.WriteAllText(@"C:\Users\ericm\source\repos\Jean - Luc Picard’s journal" + currentDate + ".txt", header + body + footer);
+
