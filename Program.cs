f36749bc7022a3797b1590b0c4a35c82e1f1f4fc
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2023-02-07 15:59:24 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Journal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> content = new List<string>();
+            bool startWriting = false;
+
+            Console.WriteLine("Enter 'start' to start writing in the journal.");
+            Console.WriteLine("Enter 'stop' to stop writing in the journal.");
+
+            while (true)
+            {
+                string line = Console.ReadLine().Trim();
+                if (line == "start")
+                {
+                    startWriting = true;
+                }
+                else if (line == "stop")
+                {
+                    startWriting = false;
+                    break;
+                }
+                else if (startWriting)
+                {
+                    content.Add(line);
+                }
+            }
+
+            string currentDate = DateTime.Now.ToString("yyyy-MM-dd");
+            //string fileName = @"C:\Users\ericm\source\repos\Jean - Luc Picard’s journal" + currentDate + ".txt";
+
+            string header = "Captain's log\nStardate " + currentDate + "\n";
+            string body = string.Join("\n", content.ToArray());
+            string footer = "\nJean-Luc Picard";
+
+            File.WriteAllText(@"C:\Users\ericm\source\repos\Jean - Luc Picard’s journal" + currentDate + ".txt", header + body + footer);
+        }
+    }
+}
